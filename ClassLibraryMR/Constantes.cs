f9d583280d32bd9023477e9543f2f9fac0672afa
Maxime Rogogine <rogogine.maxime@gmail.com>
2020-04-17 09:49:08 +0200

    Ajout de crypto

---
+++ ClassLibraryMR/Constantes.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibraryMR
+{
+    public static class Constantes
+    {
+        public const string PARAPHRASE = "j'aime_Enormement_lec#";
+    }
+}
